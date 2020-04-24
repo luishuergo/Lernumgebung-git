ae20b93c11dbb79c35b404d352038872afbb546b
Luis Huergo <30261087+luishuergo@users.noreply.github.com>
2020-04-24 14:08:32 +0200

    Die Datei "calc.py" wurde hinzugefügt

---
+++ calc.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 24 15:07:52 2020
+
+@author: saturn
+"""
+
+def add(x,y):
+    pass
+
+def subtract(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    return x/y
